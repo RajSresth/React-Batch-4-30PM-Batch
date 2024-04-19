9ee3a19a27359efb67d8443febdfe8e305f1b3f2
Shresth Rajput <rajshresth2@gmail.com>
2024-04-19 17:45:14 +0530

    Card Props

--- src/Components/Props/Props2/Card.jsx
+++ src/Components/Props/Props2/Card.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
+  
   return (
-    <div className='w-[350px] bg-slate-100 rounded-md text-center pb-5'>
+    <div className='shadow-[2px_2px_12px_3px_rgba(125,125,125)] w-[350px] h-[450px] bg-slate-100 rounded-md text-center pb-5'>
 
-          <img src='https://assets.gqindia.com/photos/5fdc6eb81c519f6c10ba2d2f/16:9/w_2560%2Cc_limit/Hrithik-Roshan%2520(2).jpg'
-            className='rounded-md'
+          <div className='h-[200px] w-[100%]'>
+          <img src={props.user.image}
+            className='rounded-md h-[100%] w-[100%] object-cover object-top'
           />
+          </div>
 
-          <h2 className='text-[1.65rem] font-semibold my-2'>Hrithik Roshan</h2>
+          <h2 className='text-[1.5rem] font-semibold my-2'>{props.user.actorName}</h2>
 
-          <p className='text-justify px-5 mb-4 text-[1.15rem]'>
-          Hrithik Roshan is an Indian actor who works in Hindi cinema. He has portrayed a variety of characters and is known for his dancing skills.
+          <p className='text-justify px-5 mb-4 text-[1rem]'>
+          {props.user.about}
           </p>
 
           <div className='flex gap-4 justify-center'>
-          <p className='rounded font-semibold py-1 bg-[#1581ab] text-white text-[1.25rem] px-5 w-[fit-content]'>
-            Krrish
+          <p className='rounded font-semibold py-1 bg-[#1581ab] text-white text-[1.15rem] px-5 w-[fit-content]'>
+           {props.user.movie2}
           </p>
           <p className='rounded font-semibold py-1 bg-[#1581ab] text-white text-[1.25rem] px-5 w-[fit-content]'>
-            War
+          {props.user.movie1}
           </p>
           <p className='rounded font-semibold py-1 bg-[#1581ab] text-white text-[1.25rem] px-5 w-[fit-content]'>
-            lakshya
+            {props.user.movie3}
           </p>
           </div> 
     </div>
