34b16dc3d260063e6f2ba485b61c4e9d753ee8ca
Shresth Rajput <rajshresth2@gmail.com>
2024-04-29 17:35:14 +0530

    UseRef Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Fragment } from 'react'
 import "./App.css";
+
 const App = () => {
-  function bgChange(){
-    document.body.style.backgroundColor='deeppink'
-  }
+    // hooks:- useRef()
+    const myRef=useRef()
 
-  const removeBg=()=>{
-    document.body.style.backgroundColor='white'
-  }
-  const changeBg=()=>{
-    const div=document.getElementById('box');
-    console.log(div)
-    div.style.backgroundColor='black'
-    div.style.cursor='pointer'
-  }
-  const previousBg=()=>{
-    const div=document.getElementById('box');
-    console.log(div)
-    div.style.backgroundColor='rgb(103 232 249)'
-    div.style.cursor='default'
-  }
-
-
-  const handleSubmit=(e)=>{
-    const input=document.getElementById('userName').value;
-    e.preventDefault()
-      console.log(e.target)
-      console.log(input)
+  const handleClick=()=>{
+    // console.log(myRef.current)
+      myRef.current.style.backgroundColor='salmon'
+   
   }
   return (
-    <>
-        {/* <button className='py-1 px-6 bg-gray-700 text-white rounded-md' onClick={bgChange} onDoubleClick={removeBg}>Dark</button> */}
-        {/* <div id="box" className='w-[25vw] h-[65vh] bg-cyan-300' onMouseEnter={changeBg} onMouseLeave={previousBg}></div> */}
-
-        <form className='flex flex-col' id='form' onSubmit={handleSubmit}>
-
-          <input type="text" id="userName" className='border border-black rounded mb-2 pl-2 focus:outline-none' placeholder='Enter Your Name' />
-
-          <button className='py-1 px-6 bg-cyan-300 rounded' id='btn'>Login</button>
-        </form>
-    </>
+    <Fragment>
+      <div className='border cursor-pointer  border-white rounded-md py-1 px-5 text-white'  onClick={handleClick} ref={myRef}>Click</div>
+    </Fragment>
   )
 }
 
