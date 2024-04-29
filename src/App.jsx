6baeba5b85bd6f0cacc4a3847b9afa244662326a
Shresth Rajput <rajshresth2@gmail.com>
2024-04-29 18:02:31 +0530

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import React, { useRef } from 'react'
-import { Fragment } from 'react'
+import React, { useState } from 'react'
 import "./App.css";
 
 const App = () => {
-    // hooks:- useRef()
-    const myRef=useRef()
+  // useState Hook
+  const [count,setCount]=useState(0)
 
-  const handleClick=()=>{
-    // console.log(myRef.current)
-      myRef.current.style.backgroundColor='salmon'
-   
+  const increment=()=>{
+    setCount(()=>count+1)
   }
+  const decrement=()=>{
+   if(count>0){
+    setCount(()=>count-1)
+   }
+  }
+  const reset=()=>{
+    setCount(()=>0)
+  }
+
   return (
-    <Fragment>
-      <div className='border cursor-pointer  border-white rounded-md py-1 px-5 text-white'  onClick={handleClick} ref={myRef}>Click</div>
-    </Fragment>
+    <div className='flex flex-col gap-8 h-screen justify-center items-center'>
+            <h1>Count: <span>{count}</span></h1>
+            <div className='flex gap-6'>
+              <button className='border-2 border-emerald-400 cursor-pointer\ py-1 px-4 rounded-md text-white' onClick={increment}>Increment</button>
+
+              <button className='border-2 border-red-400 cursor-pointer py-1 px-4 rounded-md text-white' onClick={decrement}>Decrement</button>
+
+              <button className='border-2 border-white cursor-pointer py-1 px-4 rounded-md text-white' onClick={reset}>Reset</button>
+            </div>
+    </div>
   )
 }
 
