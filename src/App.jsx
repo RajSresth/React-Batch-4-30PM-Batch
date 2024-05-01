64a5936f02b0889d895f2ef59ee6fd267f5b0562
Shresth Rajput <rajshresth2@gmail.com>
2024-05-01 18:41:50 +0530

    Use state commit second

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React, { useState } from 'react'
-import Nav from './Components/Props/stateHook/Nav'
-import Home from './Components/Props/stateHook/Home'
-import About from './Components/Props/stateHook/About'
+import "./App.css"
 
 const App = () => {
-  const [page,setPage]=useState(true)
+  const [login,setUserLogin]=useState(true)
   return (
     <div>
-      <Nav></Nav>
-      {page?<Home/>:<About/>}     
-
-      {page?<button className='px-4 py-1 bg-cyan-800 text-white' onClick={()=>setPage(false)}>About</button>:<button className='px-4 py-1 bg-cyan-800 text-white' onClick={()=>setPage(true)}>Home</button>}     
+      {login?<h1>Login</h1>:<h1>Not Login</h1>}
       
+      <button className='border-[1px] py-1 px-4 rounded-md border-black mt-5' onClick={()=>setUserLogin(false)}>Click</button>
     </div>
   )
 }
+
 export default App
