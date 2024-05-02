7492ced2dffc42093cac70e86c6a047ec2ff387c
Shresth Rajput <rajshresth2@gmail.com>
2024-05-02 17:06:27 +0530

    CBC Syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import "./App.css"
 
-const App = () => {
-  const [login,setUserLogin]=useState(true)
-  return (
-    <div>
-      {login?<h1>Login</h1>:<h1>Not Login</h1>}
-      
-      <button className='border-[1px] py-1 px-4 rounded-md border-black mt-5' onClick={()=>setUserLogin(false)}>Click</button>
-    </div>
-  )
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state={
+        userName:'Ram',
+        age:23,
+        city:'xyz'
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello React Developers...!</h1>
+        <h2>User Name is: {this.state.userName}</h2>
+        <h2>Age is: {this.state.age}</h2>
+        <h2>City is: {this.state.city}</h2>
+        <button className='bg-purple-400 text-white py-1 px-4 rounded-md mt-2' onClick={()=>this.setState({age:24,city:"Delhi"})}>Change City</button>
+      </div>
+    )
+  }
 }
-
-export default App
